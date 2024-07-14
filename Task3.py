f07a7fd2ece6e8a3da79f462bfed9fec134cc247
Rahul Kumar Saw <shawrahulkumar26@gmail.com>
2024-07-14 23:05:30 +0530

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -1,6 +1,8 @@
 # Task3: Text Generation with Markov Chains
 
-# Implement a simple text generation algorithm using Markov chains. This task involves creating a statistical model that predicts the probability of a character or word based on the previous ones(s).
+'''Problem Statement: Implement a simple text generation algorithm using Markov chains. 
+This task involves creating a statistical model that predicts the probability 
+of a character or word based on the previous ones(s).'''
 
 import random
 import re
